9bf5fcf0e9cb569db24a79924afc5a9239a3571a
Armağan Gök <1armagangok@gmail.com>
2022-11-18 18:02:06 +0300

    personal data model created

---
+++ lib/feature/personal_details/model/personal_data_model.dart
@@ -0,0 +1,17 @@
+class PersonalDataModel {
+  final String name;
+  final String location;
+  final String phoneNumber;
+  final String email;
+  final String linkedin;
+  final String birthday;
+
+  PersonalDataModel({
+    required this.name,
+    required this.location,
+    required this.phoneNumber,
+    required this.email,
+    required this.linkedin,
+    required this.birthday,
+  });
+}
