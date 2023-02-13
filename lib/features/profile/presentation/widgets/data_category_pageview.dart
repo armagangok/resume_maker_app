9971cd15911c4a6bcfe9f4057d2324ecc546309a
Armağan Gök <1armagangok@gmail.com>
2023-02-13 11:51:32 +0300

    .

--- lib/features/profile/presentation/widgets/data_category_pageview.dart
+++ lib/features/profile/presentation/widgets/data_category_pageview.dart
@@ -1,21 +1,9 @@
 import '../../../../core/export/export.dart';
 
-class DataCategoryPageView extends StatefulWidget {
+class DataCategoryPageView extends StatelessWidget {
   const DataCategoryPageView({
     super.key,
   });
-
-  @override
-  State<DataCategoryPageView> createState() => _DataCategoryPageViewState();
-}
-
-class _DataCategoryPageViewState extends State<DataCategoryPageView> {
-  final profileCubit = Injection.profileCubit;
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +13,11 @@ class _DataCategoryPageViewState extends State<DataCategoryPageView> {
         bloc: Injection.profileCubit,
         builder: (context, state) {
           return PageView.builder(
-            controller: profileCubit.controller,
-            itemBuilder: (context, position) => profileCubit.widgets[position],
-            itemCount: profileCubit.widgets.length, // Can be null
+            controller: Injection.profileCubit.controller,
+            itemBuilder: (context, position) {
+              return Injection.profileCubit.widgets[position];
+            },
+            itemCount: Injection.profileCubit.widgets.length, // Can be null
           );
         },
       ),
