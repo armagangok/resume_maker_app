7068167c323eab0b10874bc0b12e8b290b24ff14
Armağan Gök <1armagangok@gmail.com>
2023-02-25 03:49:06 +0300

    .

---
+++ lib/features/design/data/contract/design_data_source_contract.dart
@@ -0,0 +1,3 @@
+abstract class DesignDataSourceContract {
+  fetchResumeTemplates();
+}
--- lib/features/design/data/datasources/design_data_source.dart
+++ lib/features/design/data/datasources/design_data_source.dart
@@ -1,1 +1,6 @@
-class DesignDataSource {}
+import 'package:resume_maker_app/features/design/data/contract/design_data_source_contract.dart';
+
+class DesignDataSource extends DesignDataSourceContract {
+  @override
+  fetchResumeTemplates() {}
+}
